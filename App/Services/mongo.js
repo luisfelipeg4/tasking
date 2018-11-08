d248ff3997e68b0ea74a8d617c5d4d7bbc94368d
unknown <luisfelipegarciacsj@gmail.com>
2018-11-08 08:34:54 -0500

    API LISTA

--- App/Services/mongo.js
+++ App/Services/mongo.js
@@ -15,8 +15,11 @@ class CrudOperations {
   constructor() {
 
   }
-  
-  async get(query){
+  /**
+   * metodo para retornar json con un select a una tabla
+   * @param {Select a ser consultado} query 
+   */
+  async select(query){
     const connectionData = {
       user: 'luisgarcia',
       host: 'localhost',
@@ -25,7 +28,7 @@ class CrudOperations {
       port: 5432,
     }
     var pool = new pg.Pool(connectionData)
-    const consulta =[]
+
     const findAllQuery = query;
     try {
       let rows = await pool.query(findAllQuery);
@@ -34,6 +37,65 @@ class CrudOperations {
     } catch(error) {
       return error;
     }
-    
   } 
+
+  async delete(query){
+    const connectionData = {
+      user: 'luisgarcia',
+      host: 'localhost',
+      database: 'tasking',
+      password: '123',
+      port: 5432,
+    }
+    var pool = new pg.Pool(connectionData)
+
+    const findAllQuery = query;
+    try {
+      let rows = await pool.query(findAllQuery);
+      console.log(rows["rows"])
+      return rows["rows"]
+    } catch(error) {
+      return error;
+    }
+  } 
+
+  async insert(query){
+    const connectionData = {
+      user: 'luisgarcia',
+      host: 'localhost',
+      database: 'tasking',
+      password: '123',
+      port: 5432,
+    }
+    var pool = new pg.Pool(connectionData)
+    const findAllQuery = query;
+    try {
+      let rows = await pool.query(findAllQuery);
+      console.log(rows["rows"])
+      return rows["rows"]
+    } catch(error) {
+      return error;
+    }
+  }
+
+  async update(query){
+    const connectionData = {
+      user: 'luisgarcia',
+      host: 'localhost',
+      database: 'tasking',
+      password: '123',
+      port: 5432,
+    }
+    var pool = new pg.Pool(connectionData)
+    const findAllQuery = query;
+    try {
+      let rows = await pool.query(findAllQuery);
+      console.log(rows["rows"])
+      return rows["rows"]
+    } catch(error) {
+      return error;
+    }
+  }
+
+
 }
--- App/controllers/tasks.js
+++ App/controllers/tasks.js
@@ -4,26 +4,92 @@ const routes = express.Router()
 const service = require('../services/mongo')
 routes.use(express.json())
  
-var connectionString = "postgres://ocalhost:5432/tasking"
-
-const connectionData = {
-    user: 'luisgarcia',
-    host: 'localhost',
-    database: 'tasking',
-    password: '123',
-    port: 5432,
-  }
-
-//var pool = new pg.Pool(connectionString)
-var pool = new pg.Pool(connectionData)
-
+/**
+ * metodo para retornar todas las filas de tasks
+ */
 routes.get('/', (req, res, next) => {
     
-    service.postgres().get("select * from tasks").then(function (respuesta) {
+    service.postgres().select("select id, descripcion, fecha, prioridad, lista from tasks").then(function (respuesta) {
         //lo que quiero hacer si recibo algo
         console.log(respuesta);
         res.send(respuesta);
         //catch si no recibe nada
+    }).catch(function (error) {
+        console.log(error);
+        res.send(error);
+    });
+  });
+/**
+ * Metodo para eliminar una task
+ */
+  routes.delete('/:id',(req,res,next)=>{
+    var id = req.params.id
+    var  query = "delete from tasks where id =" + id; 
+    console.log(query)
+    service.postgres().delete("delete from tasks where id =" +id ).then(function (respuesta) {
+        //lo que quiero hacer si recibo algo
+        console.log(respuesta);
+        res.send(respuesta);
+        //catch si no recibe nada
+    }).catch(function (error) {
+        console.log(error);
+        res.send(error);
+    });
+  });
+  
+  routes.post('/',(req,res,next)=>{
+
+    const task ={id : req.body.id,
+        descripcion: req.body.descripcion,
+        fecha : req.body.fecha,
+        prioridad: req.body.prioridad,
+        lista : req.body.lista
+    };
+    
+    console.log (task)
+
+    var query = "INSERT INTO tasks (id,descripcion,fecha,prioridad,lista) values (" + task.id+",'" + task.descripcion  +"','"+ task.fecha + "'," + task.prioridad+","+ task.lista+ ")"
+   console.log(query)
+    service.postgres().insert(query).then(function (respuesta) {
+        //lo que quiero hacer si recibo algo
+        console.log(respuesta);
+        res.send(respuesta);
+        //catch si no recibe nada
+    }).catch(function (error) {
+        console.log(error);
+        res.send(error); 
+    });
+  });
+
+
+  /**
+   * Metodo para cambiar de estado una tarea 
+   */
+  routes.put('/:id',(req,res,next)=>{
+
+    var id = req.params.id
+    
+    var query = "SELECT lista FROM tasks where id =" + id 
+    console.log(query)
+    service.postgres().select(query).then(function (respuesta) {
+    
+        if ( respuesta[0].lista== false) {
+            var query2= "UPDATE tasks SET lista ="+true +" where id = " + id;
+            service.postgres().update(query2).then(function (respuesta){
+                res.send("cambia a true")
+            }).catch(function(error){
+                console.log(error)
+                res.send(error)
+            });
+        }else {
+            var query2= "UPDATE tasks SET lista ="+false +" where id = " + id;
+            service.postgres().update(query2).then(function (respuesta){
+                res.send("cambia a false")
+            }).catch(function(error){
+                console.log(error)
+                res.send(error)
+            });
+        }
     }).catch(function (error) {
         console.log(error);
         res.send(error);
